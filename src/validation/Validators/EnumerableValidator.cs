d232666a8783c9d4520896522b698c332f552e49
Rodrigo Daros <rodrigo.conspena@gmail.com>
2020-04-11 18:24:37 -0300

    Adição de validação para enumerados.

--- src/validation/Validators/EnumerableValidator.cs
+++ src/validation/Validators/EnumerableValidator.cs
@@ -31,6 +31,12 @@ namespace Validation.Validators
             return this;
         }
 
+        public EnumerableValidator Contains(object value, string error = DefaultError, params object[] args)
+        {
+            Execute(() => values?.Contains(value) ?? false, error, args);
+            return this;
+        }
+
         public EnumerableValidator HaveCount(int count, string error = DefaultError, params object[] args)
         {
             Execute(() => values?.Count() == count, error, args);
@@ -76,6 +82,12 @@ namespace Validation.Validators
         public EnumerableValidator NotBeNullOrEmpty(string error = DefaultError, params object[] args)
         {
             Execute(() => (values?.Count() ?? -1) > 0, error, args);
+            return this;
+        }
+
+        public EnumerableValidator NotContains(object value, string error = DefaultError, params object[] args)
+        {
+            Execute(() => !values?.Contains(value) ?? false, error, args);
             return this;
         }
     }
